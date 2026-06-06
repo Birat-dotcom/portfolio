674fc3ce3ff003186f7824118ff4eef0adf5349f
Birat-dotcom <khadkabirat21@gmail.com>
2026-06-06 17:51:19 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-// when page opens
-window.onload = function () {
-  showCart();
-};
-/* ---------- CART ---------- */
-let total = 0;
-function addToCart(name, price) {
-  total = total + price;
-  alert(name + " added");
-  showCart();
-}
-function showCart() {
-  let t = document.getElementById("orderTotal");
-  if (t) {
-    t.innerText = "Total: Rs " + total;
-  }
-}
-/* ---------- BOOK TABLE ---------- */
-function bookTable() {
-  let name = document.getElementById("rname").value;
-  let date = document.getElementById("rdate").value;
-  let time = document.getElementById("rtime").value;
-
-  if (name == "" || date == "" || time == "") {
-    alert("Fill all fields");
-    return;
-  }
-  alert("Table booked for " + name);
-}
 /* ---------- CONTACT ---------- */
 function sendMessage() {
     let name = document.getElementById("cname").value;
@@ -42,29 +13,4 @@ function sendMessage() {
         return;
     }
     alert("Message sent");
-}
-/* ---------- LOGIN ---------- */
-function login() {
-  let email = document.getElementById("email").value;
-  let pass = document.getElementById("password").value;
-  // patterns (FIXED NAMES)
-  let emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
-  let passPattern = /^(?=.*[a-zA-Z])(?=.*[0-9]).{6,}$/;
-  // 1. empty check first
-  if (email == "" || pass == "") {
-    alert("Fill all fields");
-    return;
-  }
-  // 2. email check
-  if (!emailPattern.test(email)) {
-    alert("Invalid email format");
-    return;
-  }
-  // 3. password check
-  if (!passPattern.test(pass)) {
-    alert("Password must be 6+ chars with letters + numbers");
-    return;
-  }
-
-  alert("Login successful");
 }
