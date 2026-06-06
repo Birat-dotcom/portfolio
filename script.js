9e563756add536444aea8603d5f78b210b47d4e8
Birat-dotcom <khadkabirat21@gmail.com>
2026-06-06 17:49:52 +0545

    Update script.js

--- script.js
+++ script.js
@@ -29,14 +29,19 @@ function bookTable() {
 }
 /* ---------- CONTACT ---------- */
 function sendMessage() {
-  let name = document.getElementById("cname").value;
-  let email = document.getElementById("cemail").value;
-  let msg = document.getElementById("cmsg").value;
-  if (name == "" || email == "" || msg == "") {
-    alert("Fill all fields");
-    return;
-  }
-  alert("Message sent");
+    let name = document.getElementById("cname").value;
+    let email = document.getElementById("cemail").value;
+    let msg = document.getElementById("cmsg").value;
+    let emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+    if (!emailPattern.test(email)) {
+        alert("Invalid email format");
+        return;
+    }
+    if (name == "" || email == "" || msg == "") {
+        alert("Fill all fields");
+        return;
+    }
+    alert("Message sent");
 }
 /* ---------- LOGIN ---------- */
 function login() {
